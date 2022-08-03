d5095add28eeb873fe9b12294725a944b9ffd5f9
LucasBara-dising <lucas.barauna098@gmial.com>
2022-08-03 08:21:54 -0300

    Configuração inicial

---
+++ AppAutenticacao/App_Start/Startup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Threading.Tasks;
+
+[assembly: OwinStartup(typeof(AppAutenticacao.App_Start.Startup))]
+
+namespace AppAutenticacao.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "AppAplicationCookie",
+                LoginPath = new PathString("Autenticacao/Login")
+            });
+
+        }
+    }
+}
---
+++ AppAutenticacao/Models/Usuario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AppAutenticacao.Models
+{
+    public class Usuario
+    {
+        [Display(Name = "Código")]
+        public int UsuarioID { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required]
+        [MaxLength(100)]
+        public string UsuNome { get; set; }
+
+        [Display(Name = "Login")]
+        [Required]
+        [MaxLength(50)]
+        public string Login { get; set; }
+
+        [Display(Name = "Senha")]
+        [Required]
+        [MaxLength(100)]
+        public string Senha { get; set; }
+    }
+}
